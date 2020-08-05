5700c1d2db4d67a46f9655415aaea7bc399bb485
chris03-dev <69199285+chris03-dev@users.noreply.github.com>
2020-08-05 22:52:47 +0800

    Update flat.cpp
    Features
    - Added support for global variable declaration
    - Added support for options after arithmetic parsing to add, subtract, multiply, and divide to value
    
    Improvements
    - Slightly optimized stack variable declaration
    - Other minor changes

--- src/flat.cpp
+++ src/flat.cpp
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
 		outdata << "section '.idata' data readable import" << endl;
 		outdata << "include \"win64a.inc\"" << endl;
         outdata << "library kernel32, 'kernel32.dll', \\" << endl;
-        outdata << "		msvcrt,   'msvcrt.dll'" << endl;
+        outdata << "        msvcrt,   'msvcrt.dll'" << endl;
         outdata << "import kernel32, ExitProcess, 'ExitProcess'" << endl;
         outdata << "import msvcrt, printf, 'printf'" << endl;
 		
@@ -90,6 +90,33 @@ int main(int argc, char **argv) {
 				cout << "Inc scope." << endl;
 			}
 			
+			//Global variable declaration
+			if (scope == "G_") {
+				if (linetmp.substr(0, 2) == "d8") {
+					string varname = scope + lexer(nextword(line, 2));
+					
+					cdata.d8m.insert(pair<string, string>(varname, (string) to_string(stacklv)));
+				}
+				if (linetmp.substr(0, 3) == "d16") {
+					string varname = scope + lexer(nextword(line, 3));
+					cout << "d16 " << varname << "\n";
+					
+					cdata.d16m.insert(pair<string, string>(varname, (string) to_string(stacklv)));
+				}
+				if (linetmp.substr(0, 3) == "d32") {
+					string varname = scope + lexer(nextword(line, 3));
+					cout << "d32 " << varname << "\n";
+					
+					cdata.d32m.insert(pair<string, string>(varname, (string) to_string(stacklv)));
+				}
+				if (linetmp.substr(0, 3) == "d64") {
+					string varname = scope + lexer(nextword(line, 3));
+					cout << "d64 " << varname << "\n";
+					
+					cdata.d64m.insert(pair<string, string>(varname, (string) to_string(stacklv)));
+				}
+			}
+			
 			//Check if line is object declaration
 			//Code is in dump.txt. DO NOT REMOVE.
 			
@@ -107,42 +134,37 @@ int main(int argc, char **argv) {
 				
 				//Set to default
 				stacklv = 0;
-				int	scopelvtmp = 1;
+				int scopelvtmp = 1;
 				
 				auto itfn = next(fstr.begin(), flinenum + 1);
 				
 				//Check end of function
 				while (scopelvtmp > 0) {
-					string fnline = nextword(*itfn, 0);
+					string 
+						fnline = nextword(*itfn, 0);
+						varname;
 					
-					//Data initialization
+					//Stack variable declaration
 					if (fnline.substr(0, 2) == "d8") {
-						string varname;
 						varname = scope + lexer(nextword(fnline, 2));
 						
 						stacklv++;
 						cdata.d8m.insert(pair<string, string>(varname, (string) to_string(stacklv)));
 					}
 					if (fnline.substr(0, 3) == "d16") {
-						string varname;
 						varname = scope + lexer(nextword(fnline, 3));
-						cout << "d16 " << varname << "\n";
 						
 						stacklv += 2;
 						cdata.d16m.insert(pair<string, string>(varname, (string) to_string(stacklv)));
 					}
 					if (fnline.substr(0, 3) == "d32") {
-						string varname;
 						varname = scope + lexer(nextword(fnline, 3));
-						cout << "d32 " << varname << "\n";
 						
 						stacklv += 4;
 						cdata.d32m.insert(pair<string, string>(varname, (string) to_string(stacklv)));
 					}
 					if (fnline.substr(0, 3) == "d64") {
-						string varname;
 						varname = scope + lexer(nextword(fnline, 3));
-						cout << "d64 " << varname << "\n";
 						
 						stacklv += 8;
 						cdata.d64m.insert(pair<string, string>(varname, (string) to_string(stacklv)));
@@ -167,26 +189,19 @@ int main(int argc, char **argv) {
 				}
 			}
 			
-			//Get lexer definitions and operations
+			//Arithmetic parsing
 			string equ = lexer(nextword(linetmp.substr(lexer(linetmp).size(), linetmp.size() - lexer(linetmp).size()), 0));
 			if (equ.find('=') < equ.size()) {
-				/*
 				string 
 					rscope,
 					lvalue,				//Get left variable of operation
-					rvalue,				//Get right variable of operation
-					varsize,			//Set size of lvalue
-					reg_ax,				//ax register size
-					reg_bx;				//bx register size
+					reg_ax;				//ax register size
 				
 				rscope = scope;
 				lvalue = lexer(linetmp);
-				rvalue = lexer(nextword(nextword(linetmp, lvalue.size()), equ.size()));
 				linetmp = nextword(nextword(linetmp, lvalue.size()), equ.size());
 				
-				bool 
-					isnum = isnumeric(rvalue),		//Determine if rvalue is a numeric constant
-					isglobal[2] = {0, 0};			//Determine if lvalue and rvalue is global or stack variable
+				bool isglobal = 0;			//Determine if lvalue is global or stack variable
 				
 				//Check if lvalue is global or stack variable
 				if (cdata.d8m.count("G_" + lvalue)
@@ -194,8 +209,7 @@ int main(int argc, char **argv) {
 				or cdata.d32m.count("G_" + lvalue)
 				or cdata.d64m.count("G_" + lvalue))
 					isglobal[0] = 1;
-				
-				
+
 				else if (
 				not (cdata.d8m.count(scope + lvalue)
 				or cdata.d16m.count(scope + lvalue)
@@ -206,217 +220,31 @@ int main(int argc, char **argv) {
 				}
 				
 				//Check if rvalue is global or stack variable
-				if (not isnum) {
-					if (cdata.d8m.count("G_" + rvalue)
-					or cdata.d16m.count("G_" + rvalue)
-					or cdata.d32m.count("G_" + rvalue)
-					or cdata.d64m.count("G_" + rvalue))
-						isglobal[1] = 1;
-					
-					else if (
-					not (cdata.d8m.count(scope + rvalue)
-					or cdata.d16m.count(scope + rvalue)
-					or cdata.d32m.count(scope + rvalue)
-					or cdata.d64m.count(scope + rvalue))) {
-						cerr << "Error: Variable '" << rvalue << "' not found." << endl;
-						return 1;
-					}
-				}
+				if (isglobal)	lvalue = "G_" + lvalue;
+				else         	lvalue = scope + lvalue;
 				
-				//Get variable size of lvalue, and set ax register
-				if (isglobal[0]) {
-					rscope = "G_";
-					if (cdata.d8m.count("G_" + lvalue)) {
-						varsize = "byte ";
-						reg_ax = "al";
-					}
-					else if (cdata.d16m.count("G_" + lvalue)) {
-						varsize = "word ";
-						reg_ax = "ax";
-					}
-					else if (cdata.d32m.count("G_" + lvalue)) {
-						varsize = "dword ";
-						reg_ax = "eax";
-					}
-					else if (cdata.d64m.count("G_" + lvalue)) {
-						varsize = "qword ";
-						reg_ax = "rax";
-					}
-				}
-				else {
-					if (cdata.d8m.count(scope + lvalue)) {
-						varsize = "byte ";
-						reg_ax = "al";
-					}
-					else if (cdata.d16m.count(scope + lvalue)) {
-						varsize = "word ";
-						reg_ax = "ax";
-					}
-					else if (cdata.d32m.count(scope + lvalue)) {
-						varsize = "dword ";
-						reg_ax = "eax";
-					}
-					else if (cdata.d64m.count(scope + lvalue)) {
-						varsize = "qword ";
-						reg_ax = "rax";
-					}
-				}
-				
-				//Get variable size of rvalue, and set bx register
-				if (not isnum) {
-					if (isglobal[1]) {
-						if (cdata.d8m.count("G_" + lvalue))			reg_bx = "bl";
-						else if (cdata.d16m.count("G_" + lvalue))	reg_bx = "bx";
-						else if (cdata.d32m.count("G_" + lvalue))	reg_bx = "ebx";
-						else if (cdata.d64m.count("G_" + lvalue))	reg_bx = "rbx";
-					}
-					else {
-						if (cdata.d8m.count(scope + lvalue))		reg_bx = "bl";
-						else if (cdata.d16m.count(scope + lvalue))	reg_bx = "bx";
-						else if (cdata.d32m.count(scope + lvalue))	reg_bx = "ebx";
-						else if (cdata.d64m.count(scope + lvalue))	reg_bx = "rbx";
-					}
-				}
-				
-				cout << "VARSZ:	" << varsize << endl;
-				
-				//Check if numeric
-				if (isnum) {
-					//List of operators
-					if (equ == "=") 
-						outtext << "mov " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					
-					if (equ == "+=")
-						outtext << "add " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					
-					if (equ == "-=")
-						outtext << "sub " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					
-					if (equ == "*=") {
-						outtext << "mov " << reg_ax << ", " << scope << lvalue << endl;
-						outtext << "mov rbx, " << rvalue << endl;
-						outtext << "mul rbx " << endl;
-						outtext << "mov " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "/=") {
-						outtext << "xor rdx, rdx" << endl;
-						outtext << "mov " << reg_ax << ", " << scope << lvalue << endl;
-						outtext << "mov rbx, " << rvalue << endl;
-						outtext << "div rbx " << endl;
-						outtext << "mov " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "&=")
-						outtext << "and " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					
-					if (equ == "!=")
-						outtext << "not " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					
-					if (equ == "|=")
-						outtext << "or " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					
-					if (equ == "%=")
-						outtext << "xor " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-				}
-				else {
-					if (equ == "=") {
-						outtext << "mov " << reg_ax << ", [" << scope << rvalue << "]" << endl;
-						outtext << "mov " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "+=") {
-						outtext << "mov " << reg_ax << ", [" << scope << rvalue << "]" << endl;
-						outtext << "add " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "-=") {
-						outtext << "mov " << reg_ax << ", [" << scope << rvalue << "]" << endl;
-						outtext << "sub " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "*=") {
-						outtext << "mov " << reg_ax << ", [" << scope << lvalue << "]" << endl;
-						outtext << "mov rbx, [" << rscope << rvalue << "]" << endl;
-						outtext << "mul rbx " << endl;
-						outtext << "mov " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "/=") {
-						outtext << "mov " << reg_ax << ", [" << scope << lvalue << "]" << endl;
-						outtext << "mov rbx, [" << rscope << rvalue << "]" << endl;
-						outtext << "div rbx " << endl;
-						outtext << "mov " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "&=") {
-						outtext << "mov " << reg_ax << ", [" << scope << rvalue << "]" << endl;
-						outtext << "and " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "!=") {
-						outtext << "mov " << reg_ax << ", [" << scope << rvalue << "]" << endl;
-						outtext << "not " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "|=") {
-						outtext << "mov " << reg_ax << ", [" << scope << rvalue << "]" << endl;
-						outtext << "or " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-					if (equ == "%=") {
-						outtext << "mov " << reg_ax << ", [" << scope << rvalue << "]" << endl;
-						outtext << "xor " << varsize << "[" << scope << lvalue << "], " << reg_ax << endl;
-					}
-				}
-				*/
-				string 
-					rscope,
-					lvalue,				//Get left variable of operation
-					rvalue,				//Get right variable of operation
-					varsize;			//Set size of lvalue
-				//	reg_ax,				//ax register size
-				//	reg_bx;				//bx register size
-				
-				rscope = scope;
-				lvalue = lexer(linetmp);
-				rvalue = lexer(nextword(nextword(linetmp, lvalue.size()), equ.size()));
-				linetmp = nextword(nextword(linetmp, lvalue.size()), equ.size());
-				
-				cout << "LINETMP:	" << linetmp << endl;
-				
-				bool 
-					isnum = isnumeric(rvalue),		//Determine if rvalue is a numeric constant
-					isglobal[2] = {0, 0};			//Determine if lvalue and rvalue is global or stack variable
-				
-				//Check if lvalue is global or stack variable
-				{
-					if (cdata.d8m.count("G_" + lvalue)
-					or cdata.d16m.count("G_" + lvalue)
-					or cdata.d32m.count("G_" + lvalue)
-					or cdata.d64m.count("G_" + lvalue))
-						isglobal[0] = 1;
-					
-					else if (
-					not (cdata.d8m.count(scope + lvalue)
-					or cdata.d16m.count(scope + lvalue)
-					or cdata.d32m.count(scope + lvalue)
-					or cdata.d64m.count(scope + lvalue))) {
-						cerr << "Error: Variable '" << lvalue << "' not found." << endl;
-						return 1;
-					}
-				}
-				//Check if rvalue is global or stack variable
-				if (not isnum) {
-					if (cdata.d8m.count("G_" + rvalue)
-					or cdata.d16m.count("G_" + rvalue)
-					or cdata.d32m.count("G_" + rvalue)
-					or cdata.d64m.count("G_" + rvalue))
-						isglobal[1] = 1;
-					
-					else if (
-					not (cdata.d8m.count(scope + rvalue)
-					or cdata.d16m.count(scope + rvalue)
-					or cdata.d32m.count(scope + rvalue)
-					or cdata.d64m.count(scope + rvalue))) {
-						cerr << "Error: Variable '" << rvalue << "' not found." << endl;
-						return 1;
-					}
-				}
+				if (cdata.d8m.count(lvalue)) 	reg_ax = "al";
+				if (cdata.d16m.count(lvalue))	reg_ax = "ax";
+				if (cdata.d32m.count(lvalue))	reg_ax = "eax";
+				if (cdata.d64m.count(lvalue))	reg_ax = "rax";
 				
 				//Parse the equation
 				lexereq(outtext, cdata, linetmp, scope);
-				//outtext << "mov rax, " << rvalue;
+				
+				if (equ == "=") 	outtext << "mov " << "[" << lvalue << "], " << reg_ax << endl;
+				if (equ == "+=")	outtext << "add " << "[" << lvalue << "], " << reg_ax << endl;
+				if (equ == "-=")	outtext << "sub " << "[" << lvalue << "], " << reg_ax << endl;
+				if (equ == "*=") {
+					outtext << "mov " << "rbx, [" << lvalue << "]" << endl;
+					outtext << "mul " << reg_ax << endl;
+					outtext << "mov " << "[" << lvalue << "], rbx" << endl;
+				}
+				if (equ == "/=") {
+					outtext << "xor rdx, rdx" << endl;
+					outtext << "mov " << "rbx, [" << lvalue << "]" << endl;
+					outtext << "div " << reg_ax << endl;
+					outtext << "mov " << "[" << lvalue << "], rbx" << endl;
+				}
 				
 				continue;
 			}
@@ -437,6 +265,8 @@ int main(int argc, char **argv) {
 					if (stacklv) {
 						outtext << "add rsp, " << stacklv << endl;
 						outtext << "pop rbp" << endl;
+						
+						stacklv = 0;
 					}
 					
 					if (linetmp.substr(0, 3) == "ret") {
@@ -459,36 +289,36 @@ int main(int argc, char **argv) {
 		//Put variables to file
 		for (auto it = cdata.d8m.begin(); it != cdata.d8m.end(); advance(it, 1)) { 
 			//If stack variable
-			if (it->first.substr(0, 2) == "F_") {
+			if (it->first.substr(0, 2) == "F_")
 				outdata << "define " << it->first << "	rbp - " << it->second << endl;
-			}
+			
 			//If global variable
 			else outdata << it->first << "\tdb 0" << endl;
 		}
 		
 		for (auto it = cdata.d16m.begin(); it != cdata.d16m.end(); advance(it, 1)) { 
 			//If stack variable
-			if (it->first.substr(0, 2) == "F_") {
+			if (it->first.substr(0, 2) == "F_")
 				outdata << "define " << it->first << "	rbp - " << it->second << endl;
-			}
+			
 			//If global variable
 			else outdata << it->first << "\tdb 0" << endl;
 		}
 		
 		for (auto it = cdata.d32m.begin(); it != cdata.d32m.end(); advance(it, 1)) { 
 			//If stack variable
-			if (it->first.substr(0, 2) == "F_") {
+			if (it->first.substr(0, 2) == "F_")
 				outdata << "define " << it->first << "	rbp - " << it->second << endl;
-			}
+			
 			//If global variable
 			else outdata << it->first << "\tdb 0" << endl;
 		}
 		
 		for (auto it = cdata.d64m.begin(); it != cdata.d64m.end(); advance(it, 1)) { 
 			//If stack variable
-			if (it->first.substr(0, 2) == "F_") {
+			if (it->first.substr(0, 2) == "F_")
 				outdata << "define " << it->first << "	rbp - " << it->second << endl;
-			}
+			
 			//If global variable
 			else outdata << it->first << "\tdb 0" << endl;
 		}
