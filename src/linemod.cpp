4abd476da66f83f6057ce939c6c156c0fe6cc517
chris03-dev <chris3_dev@protonmail.com>
2020-08-08 12:45:44 +0800

    Update linemod.cpp

--- src/linemod.cpp
+++ src/linemod.cpp
@@ -1,6 +1,6 @@
 /*
-   The following source code is licensed under the BSD 2-Clause Patent License.\
-   Please read LICENSE.md for details.
+	The following source code is licensed under the BSD 2-Clause Patent License.\
+	Please read LICENSE.md for details.
 */
 
 #include <iostream>
