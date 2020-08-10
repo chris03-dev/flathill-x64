9c95df5f26ca4643494b902b907f6a2a7734b7d3
chris03-dev <chris3_dev@protonmail.com>
2020-08-10 14:31:21 +0800

    Added icon and description in Win32 variant of binary

--- src/win-compile.bat
+++ src/win-compile.bat
@@ -1,6 +1,7 @@
 @echo off
+windres win32res.rc -O coff -o win32res.res
 g++ -Wall -O3 -Os -std=c++11 -c linemod.cpp 
 g++ -Wall -O3 -Os -std=c++11 -c flat.cpp 
-g++ linemod.o flat.o -s -o ../bin/flat.exe
+g++ linemod.o flat.o win32res.res -s -o ../bin/flat.exe
 
-cmd /c del /f linemod.o flat.o
+cmd /c del /f linemod.o flat.o win32res.res
